17edea9158068e267c36e589061f28345ff7ea65
Braeden Gilbert <noemail>
2025-10-28 22:47:02 -0500

    Initial Commit

---
+++ How To/How_ToApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_ToApp.swift
+//  How To
+//
+//  Created by braeden gilbert on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct How_ToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ How ToTests/How_ToTests.swift
@@ -0,0 +1,17 @@
+//
+//  How_ToTests.swift
+//  How ToTests
+//
+//  Created by braeden gilbert on 10/28/25.
+//
+
+import Testing
+@testable import How_To
+
+struct How_ToTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
